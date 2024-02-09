cc2545afa7bcb938e37bf54ace68ff23a743bbde
Tiina Kuustie <tiina.kuustie@gmail.com>
2024-02-09 14:46:17 +0200

    01 palautus

---
+++ 01palautusTestausBasic01/MathOperations.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace TestausBasic01
+{
+    internal class MathOperations
+    {
+        static void Main(string[] args)
+        {
+            Ohjelma x = new Ohjelma();
+            //    x.Kokonaisluku(2);
+
+            //x.Neliojuuri(4);
+
+            List<double> lista = new List<double> { 5.5, 3.3, 8.8, 2.2, 7.7, 1.1 };
+
+
+            double result = x.Listanpienin(lista);
+
+            Console.WriteLine($"Listan pienin arvo on: {result}");
+        }
+
+       
+    }
+}
---
+++ 01palautusTestausBasic01/Ohjelma.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestausBasic01
+{
+    public class Ohjelma
+    {
+        public double lasku2;
+        public double arvoI;
+
+        public int KaksiLukua(int eka, int toka)
+        {
+            int lasku = eka - toka;
+
+            return lasku;
+        }
+
+       public double Kokonaisluku(int luku)
+        {
+            lasku2 = 0;
+           
+            if (luku < 101)
+            {
+                lasku2 = Math.Pow(luku, 2);
+             //   Console.WriteLine(lasku2);
+            }
+            
+
+            return lasku2;
+        }
+
+
+        public double Neliojuuri(int luku)
+        {
+
+         
+            double lasku3 = Math.Sqrt(luku);
+            Console.WriteLine($"Neliöjuuri luvusta {luku} on {lasku3}");
+
+            return lasku3;
+
+        }
+
+        public double Listanpienin(List<double> sisaanlista) //Tee ohjelma, joka etsii double tyyppisestä listasta (List) pienimmän arvon ja palauttaa sen.
+                                        //Tee itse pienimmän arvon etsiminen. List.Min metodin käyttö on kielletty. Tee ohjelmalle yksikköstestit.
+        {
+        
+            arvoI = sisaanlista[0];
+          
+
+            for (int i= 0; i < sisaanlista.Count;i++)
+            {
+                double arvo = sisaanlista[i];
+                if (arvo < arvoI) arvoI = arvo;
+                
+            }
+
+            double listanpienin = arvoI;
+            Console.WriteLine($"Listan pienin arvo on: {listanpienin}");
+            return listanpienin;
+
+
+
+        }
+
+        public int ListanSuurin(List<int> sisaanlista) //Tee ohjelma, joka etsii double tyyppisestä listasta (List) pienimmän arvon ja palauttaa sen.
+                                                             //Tee itse pienimmän arvon etsiminen. List.Min metodin käyttö on kielletty. Tee ohjelmalle yksikköstestit.
+        {
+          
+           int arvoI2 = sisaanlista[0];
+
+
+            for (int i = 0; i < sisaanlista.Count; i++)
+            {
+                int arvo = sisaanlista[i];
+                if (arvo > arvoI2) arvoI2 = arvo;
+
+            }
+
+            int listanSuurin = arvoI2;
+            Console.WriteLine($"Listan suurin arvo on: {listanSuurin}");
+            return listanSuurin;
+
+
+
+        }
+
+        public float ListanKeskiarvo(List<float> sisaanlista)
+        {
+            float keskiarvo = 0;
+
+            for (int i = 0; i < sisaanlista.Count; i++)
+            {
+                keskiarvo = keskiarvo + sisaanlista[i];
+            }
+
+            keskiarvo = keskiarvo/sisaanlista.Count;
+            Console.WriteLine($"Listan keskiarvo on: {keskiarvo}");
+
+            return keskiarvo;
+        }
+
+    }
+}
